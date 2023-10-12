9a48a9b88fd73eebf2d4dc348152dc2ab6377f58
Andre Brito <felipe.brito016@gmail.com>
2023-10-12 15:36:33 -0300

    Initial Commit

---
+++ scroll-parallax/scroll_parallaxApp.swift
@@ -0,0 +1,17 @@
+//
+//  scroll_parallaxApp.swift
+//  scroll-parallax
+//
+//  Created by André Brito on 12/10/23.
+//
+
+import SwiftUI
+
+@main
+struct scroll_parallaxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
